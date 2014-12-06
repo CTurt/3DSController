50fd42034661a9aad3f13559753328a8073472ae
CTurt <ertl.chris@gmail.com>
2014-12-06 13:01:04 +0000

    Text

--- 3DS/source/main.c
+++ 3DS/source/main.c
@@ -6,6 +6,7 @@
 
 #include "wireless.h"
 #include "settings.h"
+#include "font.h"
 
 int main(void) {
 	srvInit();
@@ -15,16 +16,56 @@ int main(void) {
 	gfxInit();
 	gfxSet3D(false);
 	
+	clearScreen();
+	drawString(10, 10, "Initing FS...");
+	gfxFlushBuffers();
+	gfxSwapBuffers();
+	
 	fsInit();
+	
+	clearScreen();
+	drawString(10, 10, "Initing SOC...");
+	gfxFlushBuffers();
+	gfxSwapBuffers();
 	
 	SOC_Initialize((u32 *)memalign(0x1000, 0x100000), 0x100000);
 	
-	if(!readSettings()) goto exit;
+	clearScreen();
+	drawString(10, 10, "Reading IP...");
+	gfxFlushBuffers();
+	gfxSwapBuffers();
 	
-	int port = DEFAULT_PORT;
+	if(!readSettings()) {
+		while(aptMainLoop()) {
+			gspWaitForVBlank();
+			hidScanInput();
+			
+			clearScreen();
+			drawString(10, 10, "Failed! Press Start and Select to exit.");
+			
+			u32 kHeld = hidKeysHeld();
+			if((kHeld & KEY_START) && (kHeld & KEY_SELECT)) goto exit;
+			
+			gfxFlushBuffers();
+			gfxSwapBuffers();
+		}
+	}
 	
-	openSocket(port);
+	clearScreen();
+	drawString(10, 10, "Connecting to %s on port %d...", settings.IPString, settings.port);
+	gfxFlushBuffers();
+	gfxSwapBuffers();
+	
+	openSocket(settings.port);
 	sendConnectionRequest();
+	
+	clearScreen();
+	gfxFlushBuffers();
+	gfxSwapBuffers();
+	
+	clearScreen();
+	gfxFlushBuffers();
+	gfxSwapBuffers();
 	
 	while(aptMainLoop()) {
 		gspWaitForVBlank();
@@ -39,7 +80,6 @@ int main(void) {
 		touchRead(&touch);
 		
 		if((kHeld & KEY_START) && (kHeld & KEY_SELECT)) break;
-		//if(cstick.dx || cstick.dy) break;
 		
 		sendKeys(kHeld, cstick, touch);
 		

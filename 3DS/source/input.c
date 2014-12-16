40d5ebb1cdfd055c35299df6b6130e82410629ca
CTurt <ertl.chris@gmail.com>
2014-12-16 17:18:19 +0000

    "input" conform to my standards

--- 3DS/source/input.c
+++ 3DS/source/input.c
@@ -4,71 +4,56 @@
 
 #include <3ds.h>
 
-#include "font.h"
+#include "drawing.h"
+
 #include "input.h"
 
 int inputIP(void) {
-	
-	touchPosition myTouchPosition;
-
-	//Pass pointer to hidTouchRead function which updates values.
-	hidTouchRead(&myTouchPosition);
-
-	//Read x cord
-	u16 posX = myTouchPosition.px;
-	//Read y cord
-	u16 posY = myTouchPosition.py;
-	
+	touchPosition touch;
+	hidTouchRead(&touch);
 	
 	//Draw Keypad
-	//Row One
-	drawString(160-20,50,"1"); //One
-	drawString(160,50,"2"); //Two
-	drawString(160+20,50,"3"); //Three
-	//Row Two
-	drawString(160-20,50+20,"4"); //Four
-	drawString(160,50+20,"5"); //Five
-	drawString(160+20,50+20,"6"); //Six
-	//Row Three
-	drawString(160-20,50+40,"7"); //Severn
-	drawString(160,50+40,"8"); //Eight
-	drawString(160+20,50+40,"9"); //Nine
-	//Row Four
-	drawString(160-10,50+60,"."); //Dot
-	drawString(160+10,50+60,"0"); //Zero
+	drawString(160-20,50, "1");
+	drawString(160, 50, "2");
+	drawString(160+20, 50, "3");
 	
+	drawString(160-20, 50+20, "4");
+	drawString(160, 50+20, "5");
+	drawString(160+20, 50+20, "6");
 	
-	//Which number pressed
-	if (posY > 50+50 && posY < 50+70) { //Bottom Strip
-		if (posX < 160 && posX > 160-20) { //Dot
-			return 11;
-		} else if (posX < 160+20 && posX > 160) { //Zero
-			return 0;
-		}
-	} else if (posX < 160-10 && posX > 160-30) { //First Column
-		if (posY < 50+10 && posY > 50-10) { //One
-			return 1;
-		} else if (posY < 50+30 && posY > 50+10) { //Four
-			return 4;
-		} else if (posY < 50+50 && posY > 50+30) { //Severn
-			return 7;
-		}
-	} else if (posX < 160+10 && posX > 160-10) { // Second Column
-		if (posY < 50+10 && posY > 50-10) { //Two
-			return 2;
-		} else if (posY < 50+30 && posY > 50+10) { //Five
-			return 5;
-		} else if (posY < 50+50 && posY > 50+30) { //Eight
-			return 8;
-		}
-	} else if (posX < 160+30 && posX > 160+10) { // Third Column
-		if (posY < 50+10 && posY > 50-10) { //Three
-			return 3;
-		} else if (posY < 50+30 && posY > 50+10) { //Six
-			return 6;
-		} else if (posY < 50+50 && posY > 50+30) { //Nine
-			return 9;
-		}
+	drawString(160-20, 50+40, "7");
+	drawString(160, 50+40, "8");
+	drawString(160+20, 50+40, "9");
+	
+	drawString(160-10, 50+60, ".");
+	drawString(160+10, 50+60, "0");
+	
+	//Bottom Strip
+	if(touch.py > 50+50 && touch.py < 50+70) {
+		if(touch.px < 160 && touch.px > 160-20) return 11; // Dot
+		else if(touch.px < 160+20 && touch.px > 160) return 0; // Zero
 	}
-	return 10;  
+	
+	//First Column
+	else if(touch.px < 160-10 && touch.px > 160-30) {
+		if(touch.py < 50+10 && touch.py > 50-10) return 1; // One
+		else if(touch.py < 50+30 && touch.py > 50+10) return 4; // Four
+		else if(touch.py < 50+50 && touch.py > 50+30) return 7; // Seven
+	}
+	
+	// Second Column
+	else if(touch.px < 160+10 && touch.px > 160-10) {
+		if(touch.py < 50+10 && touch.py > 50-10) return 2; // Two
+		else if(touch.py < 50+30 && touch.py > 50+10) return 5; // Five
+		else if(touch.py < 50+50 && touch.py > 50+30) return 8; // Eight
+	}
+	
+	// Third Column
+	else if(touch.px < 160+30 && touch.px > 160+10) {
+		if(touch.py < 50+10 && touch.py > 50-10) return 3; // Three
+		else if(touch.py < 50+30 && touch.py > 50+10) return 6; // Six
+		else if(touch.py < 50+50 && touch.py > 50+30) return 9; // Nine
+	}
+	
+	return 10;
 }

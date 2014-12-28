5edff5de4afafe7c635a383dac62ce89bf3bb33b
CTurt <cturt@users.noreply.github.com>
2014-12-28 11:23:48 +0000

    Packet changes

--- PC/include/wireless.h
+++ PC/include/wireless.h
@@ -7,23 +7,31 @@
 #include <windows.h>
 #include <winsock.h>
 
+#include <stddef.h>
+
+#define SCREENSHOT_CHUNK 4000
+
 #define IP INADDR_ANY
 
 enum NET_COMMANDS {
 	CONNECT,
 	KEYS,
+	SCREENSHOT,
 };
 
+// It is deliberately set up to have an anonymous struct as well as a named struct for convenience, not a mistake!
 struct packet {
 	struct packetHeader {
 		unsigned char command;
 		unsigned char keyboardActive;
 	};
+	struct packetHeader packetHeader;
 	
 	union {
 		// CONNECT
 		struct connectPacket {
 		};
+		struct connectPacket connectPacket;
 		
 		// KEYS
 		struct keysPacket {
@@ -39,6 +47,14 @@ struct packet {
 				unsigned short y;
 			} touch;
 		};
+		struct keysPacket keysPacket;
+		
+		// SCREENSHOT
+		struct screenshotPacket {
+			unsigned short offset;
+			unsigned char data[SCREENSHOT_CHUNK];
+		};
+		struct screenshotPacket screenshotPacket;
 	};
 };
 
@@ -57,3 +73,5 @@ void printIPs(void);
 void startListening(void);
 void sendBuffer(int length);
 int receiveBuffer(int length);
+
+void sendScreenshot(void);

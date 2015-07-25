a8778b4438c29a55fe9a53015dda469bb92646a8
CTurt <cturt@users.noreply.github.com>
2015-07-25 22:42:55 +0100

    Update for latest compilers and libraries

--- PC/include/keys.h
+++ PC/include/keys.h
@@ -1,6 +1,19 @@
 #pragma once
 
 #include <windows.h>
+#include <winable.h>
+#include <winuser.h>
+
+// For some reason, these are not defined in winuser.h like they used to be...
+#ifndef VK_OEM_MINUS
+#define VK_OEM_MINUS 0xBD
+#endif
+#ifndef VK_OEM_COMMA
+#define VK_OEM_COMMA 0xBC
+#endif
+#ifndef KEYEVENTF_SCANCODE
+#define KEYEVENTF_SCANCODE 0x08
+#endif
 
 #define MAPVK_VK_TO_VSC 0
 
--- PC/source/keys.c
+++ PC/source/keys.c
@@ -1,5 +1,9 @@
 #include "keys.h"
 
+// Sideband comunication with vJoy Device
+//{781EF630-72B2-11d2-B852-00C04FAD5101}
+DEFINE_GUID(GUID_DEVINTERFACE_VJOY, 0x781EF630, 0x72B2, 0x11d2, 0xB8, 0x52, 0x00, 0xC0, 0x4F, 0xAD, 0x51, 0x01);
+
 unsigned int lastKeys;
 unsigned int currentKeys;
 

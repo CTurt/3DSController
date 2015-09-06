dc7f9f4661206f14736673c1c54e17b5e9b55da5
CTurt <cturt@users.noreply.github.com>
2015-09-06 18:50:21 +0100

    backlight

--- 3DS/include/drawing.h
+++ 3DS/include/drawing.h
@@ -1,5 +1,13 @@
 #pragma once
 
+#ifndef REG_LCDBACKLIGHTMAIN
+#define REG_LCDBACKLIGHTMAIN (u32)(0x1ED02240 - 0x1EB00000)
+#endif
+
+#ifndef REG_LCDBACKLIGHTSUB
+#define REG_LCDBACKLIGHTSUB (u32)(0x1ED02A40 - 0x1EB00000)
+#endif
+
 inline void clearScreen(void);
 
 #define drawPixelRGB(x, y, r, g, b) drawPixelRGBFramebuffer(0, x, y, r, g, b)
@@ -10,3 +18,6 @@ inline void drawBoxFramebuffer(u8 *fb, int x, int y, int width, int height, u8 r
 
 #define drawString(sx, sy, text, ...) drawStringFramebuffer(0, sx, sy, text, ##__VA_ARGS__)
 void drawStringFramebuffer(u8 *fb, int sx, int sy, char *text, ...);
+
+void disableBacklight();
+void enableBacklight();
